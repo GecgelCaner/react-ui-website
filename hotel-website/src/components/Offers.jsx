b198fedcf0aebc6b63f0b07193376ee6c54b7e78
GecgelCaner <gecgelcaner.10@gmail.com>
2023-08-21 13:40:32 +0300

    folder change

---
+++ hotel-website/src/components/Offers.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Offers = () => {
+  return (
+    <div className="max-w-[900pz] m-auto px-4 py-12 flex flex-wrap justify-between">
+      <p className="text-lg font-bold text-gray-500 ">Booking Incentives</p>
+      <p className="text-lg font-bold text-gray-500 ">Competitive Rates</p>
+      <p className="text-lg font-bold text-gray-500 ">Book With Flexibility</p>
+      <p className="text-lg font-bold text-gray-500 ">Trip Insurance</p>
+    </div>
+  );
+};
+
+export default Offers;
